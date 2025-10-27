1c4a44efdf570d4897febf6426cf98578bb3900a
Jan <7893682+janstuemmel@users.noreply.github.com>
2025-10-27 13:42:54 +0100

    add dev documentation

---
+++ README.md
@@ -0,0 +1,11 @@
+# tools
+
+a repository for tools i personally use. 
+
+written in plain javascript with dependencies from [esm.sh](https://esm.sh). no compiliation or bundeling.
+
+## dev
+
+```sh
+deno task start
+```
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ Deno.serve({ port: 1337 }, async (req) => {
   return await Deno.stat(path)
     .then((stat) => stat.isDirectory ? `${path}/index.html` : path)
     .then(Deno.readTextFile)
+    // turn on input autocompletion to save state after browser refresh for better dev expierence
     .then((file) => file.replaceAll('autocomplete="off"', 'autocomplete="on"'))
     .then((file) => new Response(file, { headers: { "content-type": mime } }))
     .catch(() => new Response("not found", { status: 404 }));
