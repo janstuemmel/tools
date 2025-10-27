b001a5edc626ace0df7f378e3c5ab36142e46471
Jan <7893682+janstuemmel@users.noreply.github.com>
2025-10-27 13:30:57 +0100

    remove file server dependency from deno

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
-
 Deno.serve({ port: 1337 }, async (req) => {
-  const res = await serveDir(req);
-  const { headers, status } = res;
-
-  // enable autocomplete to have a better developer experience
-  if (headers.get("content-type")?.includes("text/html")) {
-    const html = await res.text();
-    const modified = html.replaceAll('autocomplete="off"', 'autocomplete="on"');
-    return new Response(modified, { status, headers });
-  }
-
-  return res;
+  const path = `.${new URL(req.url).pathname}`;
+  const mime = path.endsWith(".css") ? "text/css" : path.endsWith(".js") ? "text/javascript" : "text/html";
+  return await Deno.stat(path)
+    .then((stat) => stat.isDirectory ? `${path}/index.html` : path)
+    .then(Deno.readTextFile)
+    .then((file) => file.replaceAll('autocomplete="off"', 'autocomplete="on"'))
+    .then((file) => new Response(file, { headers: { "content-type": mime } }))
+    .catch(() => new Response("not found", { status: 404 }));
 });
