a39a583ea0941265a40a7b9c7ecccd13a4a56ddf
Jan <7893682+janstuemmel@users.noreply.github.com>
2025-10-28 21:15:27 +0100

    add compile binary task

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 Deno.serve({ port: 1337 }, async (req) => {
-  const path = `./docs${new URL(req.url).pathname}`;
+  const path = `${import.meta.dirname}/docs${new URL(req.url).pathname}`;
   const mime = path.endsWith(".css") ? "text/css" : path.endsWith(".js") ? "text/javascript" : "text/html";
   return await Deno.stat(path)
     .then((stat) => stat.isDirectory ? `${path}/index.html` : path)
